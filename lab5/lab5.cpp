2dd177b4e628152b7ae8df109aa9f1c80832d844
olnkov <oleynikovsasha2006@gmail.com>
2026-03-05 11:04:37 +1000

    test

---
+++ lab5/lab5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string> 
+using namespace std;
+
+//структура 
+struct Patient
+{
+	string name;
+	string surname;
+	string birth;
+
+};
+int main() 
+{
+	ifstream file("patients.txt");
+
+	string line;
+	while (getline(file, line)) {
+	}
+}
